f2fec47905f2f87c1cd0233ccb0a1fbd4a080a10
kfoolable <kgg.ggk.kfool@gmail.com>
2024-02-07 03:16:24 +0800

    Fixing about me section

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 'use strict';
 
+const bottomHeroSection = document.querySelector('.section-bottom-hero');
 const aboutMeSection = document.querySelector('.section-about-me');
+
+const divider = document.querySelector('.divider');
+let lastScrollPos = 10;
 
 window.addEventListener('scroll', function () {
   const scrollPos = window.scrollY;
-  let lastScrollPos = 0;
 
-  if (scrollPos > 15) {
-    if (scrollPos > lastScrollPos) {
-      aboutMeSection.classList.add('scroll-trigger');
-    }
+  if (scrollPos > lastScrollPos) {
+    bottomHeroSection.classList.add('scroll-trigger');
+    aboutMeSection.classList.add('open');
   } else {
-    aboutMeSection.classList.remove('scroll-trigger');
+    bottomHeroSection.classList.remove('scroll-trigger');
+    aboutMeSection.classList.remove('open');
   }
 
-  // Update the last scroll poWsition
-  lastScrollPos = scrollPos;
+  // Get the distance between the top of the viewport and the top of the divider element
+  const distanceToDivider = divider.getBoundingClientRect().top;
+
+  aboutMeSection.style.transform = `translateY(-${distanceToDivider}px)`;
 });
+
+const isInViewport = function (element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
 
 console.log('Hello');
