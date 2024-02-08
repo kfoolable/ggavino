705556f94a4ab27734e1b91db59660081c514621
kfoolable <kgg.ggk.kfool@gmail.com>
2024-02-09 02:45:59 +0800

    Finished hero page

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const bottomHeroSection = document.querySelector('.section-bottom-hero');
 const aboutMeSection = document.querySelector('.section-about-me');
 
 const divider = document.querySelector('.divider');
-let lastScrollPos = 10;
+
+let minScrollPos = 0;
 
 window.addEventListener('scroll', function () {
   const scrollPos = window.scrollY;
+  // console.log(scrollPos);
 
-  if (scrollPos > lastScrollPos) {
+  if (scrollPos > minScrollPos) {
     bottomHeroSection.classList.add('scroll-trigger');
     aboutMeSection.classList.add('open');
   } else {
@@ -17,21 +19,7 @@ window.addEventListener('scroll', function () {
     aboutMeSection.classList.remove('open');
   }
 
-  // Get the distance between the top of the viewport and the top of the divider element
-  const distanceToDivider = divider.getBoundingClientRect().top;
-
-  aboutMeSection.style.transform = `translateY(-${distanceToDivider}px)`;
+  console.log(scrollPos);
 });
-
-const isInViewport = function (element) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
 
 console.log('Hello');
