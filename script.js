6168e035f6f01936404a9d126641881babba6447
kfoolable <kgg.ggk.kfool@gmail.com>
2024-02-12 17:30:56 +0800

    Fixed section about me animation logic

--- script.js
+++ script.js
@@ -10,14 +10,34 @@ let minScrollPos = 10;
 window.addEventListener('scroll', function () {
   const scrollPos = window.scrollY;
   // console.log(scrollPos);
+  const dividerPos =
+    (window.innerHeight - divider.getBoundingClientRect().top) * 0.1;
 
   if (scrollPos > minScrollPos) {
     bottomHeroSection.classList.add('scroll-trigger');
     aboutMeSection.classList.add('open');
+
+    // if (dividerPos >= 0) {
+    //   aboutMeSection.style.transform = `matrix(1, 0, 0, 1, 0, -${dividerPos})`;
+    // }
+
+    if (dividerPos > 0) {
+      aboutMeSection.style.transform = `translate(-50%, ${-dividerPos + -50}%)`;
+    } else {
+      aboutMeSection.style.transform = `translate(-50%, -50%)`;
+    }
   } else {
     bottomHeroSection.classList.remove('scroll-trigger');
     aboutMeSection.classList.remove('open');
   }
+
+  // if (dividerPos >= 0) {
+  //   aboutMeSection.style.transform = `matrix(1, 0, 0, 1, 0, -${dividerPos})`;
+  // }
+
+  // if (dividerPos > 0) {
+  //   aboutMeSection.style.transform = `translateY(-${dividerPos}%)`;
+  // }
 });
 
 console.log('Hello');
