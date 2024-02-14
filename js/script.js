7205bd8790fff45a68e60af8d0a235890c3197f8
kfoolable <kgg.ggk.kfool@gmail.com>
2024-02-14 20:25:25 +0800

    Fixed active state of nav links, changed font color

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 'use strict';
 
+const heroSection = document.querySelector('.section-hero');
 const bottomHeroSection = document.querySelector('.section-bottom-hero');
 const aboutMeSection = document.querySelector('.section-about-me');
 const skillsSection = document.querySelector('.section-skills');
 const projectsSection = document.querySelector('.section-projects');
+const contactSection = document.querySelector('.section-contact');
 
+const ggTitle = document.querySelector('.kgg-title');
+const aboutEl = document.getElementById('about');
+const skillsEl = document.getElementById('skills');
+const projectsEl = document.getElementById('projects');
+const contactEl = document.getElementById('contact');
 const divider = document.querySelector('.divider');
 
 let minScrollPos = 10;
+let maxScrollPos = 50;
 
 window.addEventListener('scroll', function () {
   const scrollPos = window.scrollY;
@@ -18,13 +26,11 @@ window.addEventListener('scroll', function () {
     bottomHeroSection.classList.add('scroll-trigger');
     aboutMeSection.classList.add('open');
 
-    if (dividerPos > 0) {
+    if (dividerPos > 0)
       aboutMeSection.style.transform = `translate(-50%, ${
         -dividerPos * 0.1 + -50
       }%)`;
-    } else {
-      aboutMeSection.style.transform = `translate(-50%, -50%)`;
-    }
+    else aboutMeSection.style.transform = `translate(-50%, -50%)`;
   } else {
     bottomHeroSection.classList.remove('scroll-trigger');
     aboutMeSection.classList.remove('open');
@@ -34,22 +40,59 @@ window.addEventListener('scroll', function () {
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
+      if (entry.target === heroSection) {
+        if (entry.isIntersecting) ggTitle.classList.add('is-active');
+        else ggTitle.classList.remove('is-active');
+      }
+
+      if (entry.target === heroSection) {
+        if (entry.isIntersecting) ggTitle.classList.add('is-active');
+        else ggTitle.classList.remove('is-active');
+      }
+
+      if (entry.target === aboutMeSection) {
+        if (entry.isIntersecting) aboutEl.classList.add('is-active');
+        else aboutEl.classList.remove('is-active');
+      }
+
       if (entry.target === skillsSection) {
         if (entry.isIntersecting) {
           skillsSection.classList.add('open');
+          skillsEl.classList.add('is-active');
+        } else {
+          skillsEl.classList.remove('is-active');
         }
-      } else if (entry.target === projectsSection) {
+      }
+
+      if (entry.target === projectsSection) {
         if (entry.isIntersecting) {
           projectsSection.classList.add('open');
+          projectsEl.classList.add('is-active');
+        } else {
+          projectsEl.classList.remove('is-active');
+        }
+      }
+
+      if (entry.target === contactSection) {
+        if (entry.isIntersecting) {
+          contactEl.classList.add('is-active');
+        } else {
+          contactEl.classList.remove('is-active');
         }
       }
     });
   },
   {
     root: null,
-    threshold: 0,
+    threshold: 0.2,
   }
 );
 
-const elements = [skillsSection, projectsSection];
+const elements = [
+  heroSection,
+  aboutMeSection,
+  skillsSection,
+  projectsSection,
+  contactSection,
+];
 elements.forEach((els) => observer.observe(els));
