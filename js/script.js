a6cdfab4e67b83462cd73925acc1991f5219de34
kfoolable <kgg.ggk.kfool@gmail.com>
2024-02-15 00:34:33 +0800

    Fixed script and CSS logic

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ const projectsSection = document.querySelector('.section-projects');
 const contactSection = document.querySelector('.section-contact');
 
 const ggTitle = document.querySelector('.kgg-title');
+
+const topNav = document.querySelector('.top-nav');
+const navLinks = document.querySelectorAll('.top-nav-link');
 const aboutEl = document.getElementById('about');
 const skillsEl = document.getElementById('skills');
 const projectsEl = document.getElementById('projects');
@@ -17,12 +20,6 @@ const divider = document.querySelector('.divider');
 let minScrollPos = 10;
 let maxScrollPos = 50;
 
-window.addEventListener('scroll', scrollEvent);
-const observer = new IntersectionObserver(isActive, {
-  root: null,
-  threshold: 0.2,
-});
-
 const elements = [
   heroSection,
   aboutMeSection,
@@ -30,6 +27,12 @@ const elements = [
   projectsSection,
   contactSection,
 ];
+
+window.addEventListener('scroll', scrollEvent);
+const observer = new IntersectionObserver(isActive, {
+  root: null,
+  threshold: 0.15,
+});
 elements.forEach((els) => observer.observe(els));
 
 function scrollEvent() {
@@ -41,6 +44,8 @@ function scrollEvent() {
     bottomHeroSection.classList.add('scroll-trigger');
     aboutMeSection.classList.add('open');
 
+    topNav.classList.add('open');
+
     if (dividerPos > 0)
       aboutMeSection.style.transform = `translate(-50%, ${
         -dividerPos * 0.1 + -50
@@ -49,16 +54,12 @@ function scrollEvent() {
   } else {
     bottomHeroSection.classList.remove('scroll-trigger');
     aboutMeSection.classList.remove('open');
+    topNav.classList.remove('open');
   }
 }
 
 function isActive(entries) {
   entries.forEach((entry) => {
-    if (entry.target === heroSection) {
-      if (entry.isIntersecting) ggTitle.classList.add('is-active');
-      else ggTitle.classList.remove('is-active');
-    }
-
     if (entry.target === heroSection) {
       if (entry.isIntersecting) ggTitle.classList.add('is-active');
       else ggTitle.classList.remove('is-active');
@@ -96,3 +97,17 @@ function isActive(entries) {
     }
   });
 }
+
+navLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const id = link.getAttribute('href');
+    console.log(id);
+    const element = document.querySelector(id);
+
+    element.scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
